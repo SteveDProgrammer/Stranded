8a22f2b544c8967255197cb3e3e7671d553df6e1
Steve <rreka777@gmail.com>
2024-08-04 01:35:12 +0800

    Updated Scene Tilemaps(and Areas) + Updated missing code in EnemyWalker script

--- Assets/Scripts/EnemyPatroller.cs
+++ Assets/Scripts/EnemyPatroller.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public Transform[] patorlPoints;
+    public Transform[] patrolPoints;
     private int currPoint;
     public float moveSpeed;
     public float waitTimeAtPoint;
@@ -12,15 +12,52 @@ public class EnemyController : MonoBehaviour
     public float jumpForce;
 
     public Rigidbody2D rb2d;
+    public Animator anim;
 
-    // Start is called before the first frame update
+// Start is called before the first frame update
     void Start()
     {
+        currPoint = 0;
+        waitTimeCounter = waitTimeAtPoint;
+        foreach (Transform p in patrolPoints)
+        {
+            p.SetParent(null);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Mathf.Abs(transform.position.x - patrolPoints[currPoint].position.x) > .2f)
+        {
+            if(transform.position.x < patrolPoints[currPoint].position.x) // To the left of point
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+                rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
+            }
+            else
+            {
+                transform.localScale = Vector3.one;
+                rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
+            }
+
+            if((transform.position.y - patrolPoints[currPoint].position.y) < -0.5f && rb2d.velocity.y < 0.1f)
+            {
+                rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+            }
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(0f, rb2d.velocity.y);
+            waitTimeCounter -= Time.deltaTime;
+            if(waitTimeCounter <= 0)
+            {
+                waitTimeCounter = waitTimeAtPoint;
+                
+                currPoint++;
+                currPoint %= patrolPoints.Length;
+            }
+        }
+        anim.SetFloat("speed", rb2d.velocity.x);
     }
 }
